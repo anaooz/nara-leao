89e8c13446dbd40522e1d02203c3830e04c6ca03
Mateus <mateusmarchetti.vieira@gmail.com>
2024-03-11 11:25:44 -0300

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import HeaderNav from "./ui/headernav"
+import FooterNav from "./ui/footernav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`} >
         <HeaderNav/>
           {children}
+        <FooterNav/>
       </body>
     </html>
   );
