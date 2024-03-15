0cab04ee4d09c72a68d7d9cdae79597f5bb235a6
Mateus <mateusmarchetti.vieira@gmail.com>
2024-03-14 22:17:55 -0300

    bibliografia adicionada

--- src/app/(overview)/pesquisa/page.tsx
+++ src/app/(overview)/pesquisa/page.tsx
@@ -1,5 +1,26 @@
-export default function Page() {
+import PageName from "@/app/ui/pagename";
+
+export default function Bibliografia() {
     return(
-        <></>
+        <>
+            <PageName name="Bibliografia"/>
+            <section className="conteudo-pagina text-justify [&>p]:my-4">
+                <p><strong>BARCELOS, Jalusa.</strong> <i>CPC da UNE</i> - uma história de paixão e consciência. Rio de Janeiro: Nova Fronteira, 1994.</p>
+                <p><strong>CABRAL, Sérgio.</strong> &quot;A figura de Nara Leão&quot; In NAVES, Santuza Cambraia e DUARTE, Paulo Sérgio. (org.). <i>Do Samba-canção à Tropicália.</i> Rio de Janeiro: Relume Dumará: FAPERJ, 2003, pp.63-67.</p>
+                <p><strong>CALADO, Carlos.</strong> <i>Tropicália: a história de uma revolução musical.</i> São Paulo: Ed. 34, 1997.</p>
+                <p><strong>CASTRO, Ruy.</strong> <i>Chega de Saudade - a história e as histórias da Bossa Nova.</i> São Paulo: Companhia das Letras, 1990</p>
+                <p><strong>CONTIER, Arnaldo.</strong> &quot;Edu Lobo e Carlos Lyra: o nacional e o popular na canção de protesto&quot;. In: <i>Revista Brasileira de História</i>, v. 18 / n. 35, ANPUH/Humanitas, 1998, 13-52.</p>
+                <p><strong>HOMEM DE MELLO, José Eduardo (Zuza).</strong> <i>Música Popular Brasileira.</i> São Paulo: Melhoramentos, Editora da Universidade de São paulo, 1976.</p>
+                <p><strong>MOTTA, Nelson.</strong> <i>Noites Tropicais - solos, improvisos e memórias musicais.</i> Rio de Janeiro: Ed. Objetiva, 2000.</p>
+                <p><strong>NAPOLITANO, Marcos.</strong> &quot;A canção engajada nos anos 60&quot; In NAVES, Santuza Cambraia e DUARTE, Paulo Sérgio. (org.). <i>Do Samba-canção à Tropicália.</i> Rio de Janeiro: Relume Dumará: FAPERJ, 2003, pp.127-132.</p>
+                <p><strong>NAVES, Santuza Cambraia.</strong> <i>Da Bossa Nova à Tropicália.</i> Rio de Janeiro: Zahar, 2001.</p>
+                <p><strong>NAVES, Santuza Cambraia.</strong> &quot;Da Bossa Nova à Tropicália: contenção e excesso na música popular&quot;. in: <i>Revista Brasileira de Ciências Sociais</i>, n. 43, vol. 15, junho de 2000.</p>
+                <p><strong>SEVERIANO, Jairo e HOMEM DE MELLO, Zuza.</strong> <i>A Canção no Tempo - 85 anos de músicas brasileiras vol. 2 (1958-1985). São Paulo: Ed. 34, 1998.</i></p>
+                <p><strong>SOUZA, Tarik.</strong> &quot;Nara canta a palavra nova&quot;, in: <i>O Som Nosso de Cada Dia.</i> Porto Alegre: LPM, 1983.</p>
+                <p><strong>TINHORÃO, José Ramos.</strong> <i>História Social da Música Popular Brasileira.</i> São Paulo: Ed. 34, 1997.</p>
+                <p><strong>TINHORÃO, José Ramos.</strong> <i>Música Popular: um tema em debate.</i> São Paulo: Ed. 34, 1997.</p>
+                <p><strong>Cavalcante, Cássio.</strong> <i>Nara Leão: A Musa dos Trópicos.</i> Recife: Cepe - Companhia Editora de Pernambuco, 2008.</p>
+            </section>
+        </>
     )
 }
