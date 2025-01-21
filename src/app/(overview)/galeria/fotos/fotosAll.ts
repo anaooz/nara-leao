59a5589be22d1379755db170ea90082c7007bd0b
Mateus <mateusmarchetti.vieira@gmail.com>
2025-01-20 22:28:30 -0300

    galeria de fotos adicionada

--- src/app/(overview)/galeria/fotos/fotosAll.ts
+++ src/app/(overview)/galeria/fotos/fotosAll.ts
@@ -1,5 +1,8 @@
+import manchete759 from "@/app/imgs/revistas/manchete759.jpg"
+import { StaticImageData } from "next/image";
+
 interface Slide {
-    [key: string]: [string, number];
+    [key: string]: [string | StaticImageData, number];
 }
 
 export const fotos40: Slide = {
@@ -26,3 +29,87 @@ export const fotos50: Slide = {
     "Murilinho de Almeida, Nara e o ator norte americano Van Heflin": ["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_756.jpg", 90],
     "Nara Leão, 1958": ["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_768.jpg", 90],
 };
+
+export const fotos60: Slide = {
+    "Nara Leão se apresentando, anos 60": ["http://www.naraleao.com.br/_imgs/galeria/fotos/nl790.jpg", 90],
+    "Nara e seu violão, anos 60": ["http://www.naraleao.com.br/_imgs/galeria/fotos/nl792.jpg", 80],
+    "Nara Leão se apresentando, anos 60 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl712.jpg", 90],
+    "Nara Leão se apresentando, anos 60  ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl799.jpg", 90],
+    "Nara Leão em sessão de gravação, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl811.jpg", 90],
+    "Nara Leão, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl812.jpg", 90],
+    "Nara Leão, anos 60 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl834.jpg", 90],
+    "Nara Leão, anos 60  ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl838.jpg", 90],
+    "Nara Leão com Aramis Millarch, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl840.jpg", 90],
+    "Nara Leão, anos 60   ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_704.jpg", 90],
+    "Nara Leão entre amigos, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_763.jpg", 90],
+    "Nara Leão, anos 60    ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_773.jpg", 90],
+    "Nara Leão, anos 60     ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl791.jpg", 90],
+    "Nara Leão e violão, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl801.jpg", 90],
+    "Samuel Wainer, Nara Leão e Cacá Diegues e , anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl853.jpg", 90],
+    "Nara Leão, anos 60      ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl874.jpg", 90],
+    "Nara Leão e o violão, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl890.jpg", 90],
+    "Nara e o violão, anos 60 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl856.jpg", 90],
+    "Nara Leão recebendo o prêmio Saci de cinema no lugar de Ruy Guerra pelo filme Os Fuzis, 1963":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl809.jpg", 90],
+    "Nara Leão se apresentando com Sérgio Mendes, Tóquio, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl860.jpg", 90],
+    "Nara Leão se apresentando com Sérgio Mendes, Tóquio, 1964 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl872.jpg", 90],
+    "Nara Leão no espetáculo Opinião, dezembro de 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl709.jpg", 90],
+    "Nara Leão, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_762.jpg", 90],
+    "Chico Buarque e Nara Leão apresentando A Banda no Festival da Record, Danuza Leão ao fundo, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl714.jpg", 90],
+    "Nara Leão se apresentando, Marcos Lázaro ao fundo, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl806.jpg", 90],
+    "Nara Leão no Festival de Record, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl796.jpg", 90],
+    "Nara Leão na noite de apresentação de “A Banda”, Festival de Record, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl804.jpg", 90],
+    "Nara Leão e Chacrinha, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl848.jpg", 90],
+    "Nara Leão em fotos para o disco Vento de maio, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl802.jpg", 90],
+    "Nara Leão, Chico Buarque e Voz do Morro, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl837.jpg", 90],
+    "Nara Leão com Itala Nandi e Fernando Peixoto, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_776.jpg", 90],
+    "Nara Leão e Samuel Wainer, 1968":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_715.jpg", 90],
+    "Nara Leão em Roma, 1969":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_715.jpg", 90],
+    "Nara Leão, Guilherme Araújo e Cacá Diegues, estúdios da BBC de Londres, 1969":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl854.jpg", 90],
+    "Banden Powell, Lúcio Alves, Nara Leão e Tom Jobim, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_60.jpg", 90],
+    "Nara Leão, Silvio Caldas e Carlos Lyra, anos 60":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_602.jpg", 90],
+    "Nara Leão, anos 60       ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_603.jpg", 90],
+    "Nara Leão e seu violão em sessão de fotos, 1963":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1963.jpg", 90],
+    "Nara Leão em sua casa, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1964.jpg", 90],
+    "Nara Leão com Silvio Caldas e Carlos Lira":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19642.jpg", 90],
+    "Nara Leão e o violão, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19643.jpg", 90],
+    "Nara Leão e Zé Keti no Opinião, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19644.jpg", 90],
+    "Nara Leão, Zé Keti e João do Valle, espetáculo Opinião, 1964":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19645.jpg", 90],
+    "Nara Leão e o violão, 1965":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1965.jpg", 90],
+    "Nara Leão se apresentando, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1966.jpg", 60],
+    "Nara Leão cantando “A banda” no Festival da Record, 1966":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19662.jpg", 90],
+    "Nara Leão e Jair Rodrigues na capa da Revista do Rádio, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1967.jpg", 90],
+    "Nara Leão e Cacá Diegues em seu casamento, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19672.jpg", 90],
+    "Nara Leão e Cacá Diegues em seu casamento, 1967 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19673.jpg ", 90],
+    "Nara Leão em ensaio fotográfico, 1967":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19674.jpg", 90],
+    "Nara Leão, Caetano Veloso, Gilberto Gil e os Beat Boys no programa Divino Maravilhoso, 1968":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1968.jpg", 90],
+    "Chico Buarque e Nara Leão na casa de Chico":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1969.jpg", 90],
+    "Nara e Chico na capa da revista Manchete, nº 759 de 1966": [manchete759, 90]
+}
+
+export const fotos70: Slide = {
+    "Nara Leão, Francisco e Isabel, anos 70":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl713.jpg", 90],
+    "Nara Leão e Chico Buarque, anos 70":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl859.jpg", 90],
+    "Nara Leão em casa com seus filhos Francisco e Isabel, anos 70":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_755.jpg", 90],
+    "Nara durante as filmagens de Lira do Delírio, 1973":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl844.jpg", 90],
+    "Nara Leão com os filhos Isabel e Francisco, anos 70":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl784.jpg", 90],
+    "Nara Leão com os filhos Isabel e Francisco, anos 70 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_70.jpg", 90],
+    "Nara Leão e Chico Buarque, década de 70":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_702.jpg", 90],
+    "Nara Leão e sua filha Isabel, 1971":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1971.jpg", 90],
+    "Nara Leão e sua filha Isabel, 1971 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19712.jpg", 90],
+    "Nara Leão e Anecy Rocha, Lira do delírio, 1973":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1973.jpg", 90],
+    "Nara Leão e Elke Maravilha, 1977":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1977.jpg", 90],
+    "Nara Leão, 1977":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_19772.jpg", 90],
+    "Nara Leão, 1979":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1979.jpg", 90],
+}
+
+export const fotos80: Slide = {
+    "Nara Leão, Roberto Menescal e Chico Buarque, anos 80":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl752.jpg", 90],
+    "Nara Leão, anos 80":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl808.jpg", 90],
+    "Nara Leão, anos 80 ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl810.jpg", 90],
+    "Menescal e Nara em gravação no estúdio":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl850.jpg", 90],
+    "Nara Leão, anos 80  ":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl797.jpg", 90],
+    "Nara Leão se apresentando no Japão, 1985":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl710.jpg", 90],
+    "Nara Leão e Roberto Menescal se apresentando no Japão, 1985":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl751.jpg", 90],
+    "Nara Leão entre seus filhos Francisco e Isabel, anos 80":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_80.jpg", 90],
+    "Nara Leão e Roberto Menescal, 1987":["http://www.naraleao.com.br/_imgs/galeria/fotos/nl_1987.jpg", 90],
+}
--- src/app/(overview)/galeria/fotos/page.tsx
+++ src/app/(overview)/galeria/fotos/page.tsx
@@ -1,7 +1,7 @@
 import Carousel from "@/app/ui/carousel"
 import PageTitle from "@/app/ui/pagetitle"
 import Image from "next/image"
-import { fotos40, fotos50 } from "@/app/(overview)/galeria/fotos/fotosAll"
+import { fotos40, fotos50, fotos60, fotos70, fotos80 } from "@/app/(overview)/galeria/fotos/fotosAll"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
 export default function Fotos() {
     const anos40 = Object.entries(fotos40).map(([key, value]) => ({ key, value }))
     const anos50 = Object.entries(fotos50).map(([key, value]) => ({ key, value }))
+    const anos60 = Object.entries(fotos60).map(([key, value]) => ({ key, value }))
+    const anos70 = Object.entries(fotos70).map(([key, value]) => ({ key, value }))
+    const anos80 = Object.entries(fotos80).map(([key, value]) => ({ key, value }))
     
     return(
         <>
@@ -47,6 +50,75 @@ export default function Fotos() {
                     <Carousel slide={fotos50}>
                         <section className="grid grid-cols-8 items-center">
                             {anos50.map((image, index) => {
+                                return(
+                                    <div key={index}>
+                                        <Image
+                                            src={image.value[0]}
+                                            width={image.value[1]}
+                                            height={image.value[1]}
+                                            alt={image.key}
+                                            className="documentos-imgs"
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </section>
+                    </Carousel>
+                </section>
+                <section className="documentos-section">
+                    <div className="documentos-text">
+                        <h3>Anos 60</h3>
+                        <p>Nara faz seus primeiros shows fora do Rio de Janeiro e inicia o namoro com o cineasta Cacá Diegues.</p>
+                    </div>
+                    <Carousel slide={fotos60}>
+                    <section className="grid grid-cols-8 items-center">
+                            {anos60.map((image, index) => {
+                                return(
+                                    <div key={index}>
+                                        <Image
+                                            src={image.value[0]}
+                                            width={image.value[1]}
+                                            height={image.value[1]}
+                                            alt={image.key}
+                                            className="documentos-imgs"
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </section>
+                    </Carousel>
+                </section>
+                <section className="documentos-section">
+                    <div className="documentos-text">
+                        <h3>Anos 70</h3>
+                        <p>Vida em Paris, primeira gravidez e reencontro com a Bossa Nova.</p>
+                    </div>
+                    <Carousel slide={fotos70}>
+                        <section className="grid grid-cols-8 items-center">
+                            {anos70.map((image, index) => {
+                                return(
+                                    <div key={index}>
+                                        <Image
+                                            src={image.value[0]}
+                                            width={image.value[1]}
+                                            height={image.value[1]}
+                                            alt={image.key}
+                                            className="documentos-imgs"
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </section>
+                    </Carousel>
+                </section>
+                <section className="documentos-section">
+                    <div className="documentos-text">
+                        <h3>Anos 80</h3>
+                        <p>Anos de intensa atividade, lançando discos, dando shows e participando de especiais de televisão.</p>
+                    </div>
+                    <Carousel slide={fotos80}>
+                        <section className="grid grid-cols-8 items-center">
+                            {anos80.map((image, index) => {
                                 return(
                                     <div key={index}>
                                         <Image
